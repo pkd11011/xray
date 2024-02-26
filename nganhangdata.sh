ab12e14d379530b36f45deec6ff040fa19846d22
pkd11011 <130068719+pkd11011@users.noreply.github.com>
2024-02-26 07:37:24 +0700

    Update nganhangdata.sh

--- nganhangdata.sh
+++ nganhangdata.sh
@@ -14,7 +14,7 @@ read -p " NODE ID 2 vmess: " node_id2
 read -p " điền ip ID 2 vmess: " ip2
   [ -z "${ip2}" ] && ip2=0
 
-bash <(curl -Ls https://raw.githubusercontent.com/longyi8/XrayR/master/install.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/pkd11011/xray/main/xrayrv3.sh)
 cd /etc/XrayR
 EOF
   cat >key.pem <<EOF
